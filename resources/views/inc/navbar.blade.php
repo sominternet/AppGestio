e42e42574c599b049431c74828dbac879fffcda4
efren <info@sominternet.cat>
2019-07-29 13:23:28 +0200

    d

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -3,7 +3,7 @@
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span> </button> <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item"> <a class="nav-link" href="/index">Index</a> </li>
+            <li class="nav-item"> <a class="nav-link" href="/">Index</a> </li>
             <li class="nav-item"> <a class="nav-link " href="/repetidors">Repetidors</a> </li>
         </ul>
         <form class="form-inline mt-2 mt-md-0">
