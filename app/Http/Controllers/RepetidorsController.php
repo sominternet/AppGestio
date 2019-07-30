17b5090dea1a32f1e6f17812745035f19dc265d9
efren <info@sominternet.cat>
2019-07-30 11:49:55 +0200

    All

--- app/Http/Controllers/RepetidorsController.php
+++ app/Http/Controllers/RepetidorsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\repetidor;
 
 class RepetidorsController extends Controller
 {
--- resources/views/repetidors/index.blade.php
+++ resources/views/repetidors/index.blade.php
@@ -2,4 +2,5 @@
 
 @section('content')
     <h1>Repetidors</h1>
+    <h3>Hola</h3>
 @endsection
